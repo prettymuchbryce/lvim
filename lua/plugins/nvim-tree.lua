59796327b0d9ebff6e5948324dbecb48a4512fba
Bryce Neal <bryce@dydx.exchange>
2022-05-18 09:29:09 -0700

    Update nvim tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,10 +5,8 @@ NvimTree.config = function()
     return
   end
 
-  lvim.builtin.nvimtree.setup.side = "left"
   lvim.builtin.nvimtree.show_icons.git = 1
-  lvim.builtin.nvimtree.setup.hide_dotfiles = 0
-  lvim.builtin.nvimtree.setup.auto_close = 1
+  lvim.builtin.nvimtree.setup.view.side = "left"
   lvim.builtin.nvimtree.setup.view.width = 40
   lvim.builtin.nvimtree.setup.view.auto_resize = true
   lvim.builtin.nvimtree.setup.git.ignore = false
