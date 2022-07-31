fee6bc91c40302fc021052cc634dbf654e80ae51
Bryce Neal <brycedneal@gmail.com>
2022-07-31 11:31:19 -0400

    Neovim v0.7.2 LunarVim 71de455

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,10 +5,9 @@ NvimTree.config = function()
     return
   end
 
-  lvim.builtin.nvimtree.show_icons.git = 1
+  lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
   lvim.builtin.nvimtree.setup.view.side = "left"
   lvim.builtin.nvimtree.setup.view.width = 40
-  lvim.builtin.nvimtree.setup.view.auto_resize = true
   lvim.builtin.nvimtree.setup.git.ignore = false
   lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 end
